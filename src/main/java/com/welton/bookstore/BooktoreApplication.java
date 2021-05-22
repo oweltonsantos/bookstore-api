1cbab9281bdfa9558d841c4613e0696f84ccb154
oweltonsantos <oweltonsantos@gmail.com>
2021-05-22 13:43:17 -0300

    Initial Commit

---
+++ src/main/java/com/welton/bookstore/BooktoreApplication.java
@@ -0,0 +1,13 @@
+package com.welton.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welton/bookstore/BooktoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welton.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
